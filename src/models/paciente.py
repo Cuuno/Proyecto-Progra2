8fab5bc97d85c16d726860168a21811e608ea9ae
Cuuno <facugespinosa@gmail.com>
2025-11-06 13:25:23 -0300

    agrego paciente

---
+++ src/models/paciente.py
@@ -0,0 +1,16 @@
+from src.models.persona import Persona
+class Paciente(Persona):
+    def __init__(self, dni, nombre, apellido, nacimiento, email, cel, domicilio):
+        super().__init__(dni,nombre,apellido)
+        self.nacimiento = nacimiento
+        self.email = email
+        self.cel = cel
+        self.domicilio = domicilio
+        print (f"Objeto Paciente '{self.nombre}'creado en memoria")
+    def registrar(self):
+        pass
+    @staticmethod
+    def buscar_por_dni(dni):
+        pass
+    def get_rol(self):
+        return "Paciente"
